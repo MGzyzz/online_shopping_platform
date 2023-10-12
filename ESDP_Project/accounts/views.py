2d85175b8af603bcce0f06fe0ea8db958f751f02
Madi <madi_gaziz@mail.ru>
2023-10-12 20:34:13 +0600

    Реализовал функционал регистрации пользователя, а так же сделал вывод ошибок в случае если пользователь не ввел данные, данные так же сохраняются

--- ESDP_Project/accounts/views.py
+++ ESDP_Project/accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import views, login
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.generic import CreateView
+
+from accounts.forms import RegisterForm, LoginForm
+from accounts.models import User
+
 
 # Create your views here.
+class RegisterView(CreateView):
+    template_name = 'register.html'
+    model = User
+    form_class = RegisterForm
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+    def form_invalid(self, form):
+        return render(self.request, 'register.html', {'form': form})
