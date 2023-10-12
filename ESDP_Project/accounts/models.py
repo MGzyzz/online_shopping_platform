6c19ad4847283336713baf7710896a0abbde57b1
Madi <madi_gaziz@mail.ru>
2023-10-12 21:47:42 +0600

    #4 исправил ошибку из-за который вызывался конфликт веток

--- ESDP_Project/accounts/models.py
+++ ESDP_Project/accounts/models.py
@@ -20,6 +20,7 @@ class CustomUserManager(BaseUserManager):
 
         return self.create_user(email, password, **extra_fields)
 
+
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=50)
