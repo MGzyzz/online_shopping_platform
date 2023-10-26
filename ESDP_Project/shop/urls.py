8c87dce92d7d24a96bb91d7ee005ecce457df497
Gleb <neloxchestno@gmail.com>
2023-10-26 20:36:24 +0600

    #37 Made deatail product page

--- ESDP_Project/shop/urls.py
+++ ESDP_Project/shop/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
+from .views.product import DetailProduct
 
 urlpatterns = [
     path('create', ShopCreateView.as_view(), name='shop_create'),
@@ -11,4 +12,5 @@ urlpatterns = [
     path('product/<int:id>/delete/', DeleteProduct.as_view(), name='delete_product'),
     path('product/<int:id>/attributes/add/', AttributesCreateView.as_view(), name='add_attributes'),
     path('product/<int:id>/attributes/update/', AttributesUpdateView.as_view(), name='update_attributes'),
+    path('product/<int:id>', DetailProduct.as_view(), name='detail_product')
 ]
