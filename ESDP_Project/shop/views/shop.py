a71360944e62aaf4301970c7710534b7b5bf7e09
Valeria Malina <wollt1362@gmail.com>
2023-11-01 21:41:52 +0300

    develop merge

--- ESDP_Project/shop/views/shop.py
+++ ESDP_Project/shop/views/shop.py
@@ -4,8 +4,10 @@ from django.views.generic import TemplateView, CreateView, UpdateView, ListView,
 
 from accounts.forms import LoginForm
 from shop.forms import ShopModelForm
-from shop.models import Shop
+from shop.models import Shop, Product
 from django.urls import reverse_lazy
+
+from shop.models.bucket import Bucket
 
 
 class ShopCreateView(LoginRequiredMixin, CreateView):
@@ -53,13 +55,28 @@ class ShopListView(ListView):
     context_object_name = 'shops'
     paginate_by = 5
     extra_context = {
-        'form': LoginForm()
+        'form': LoginForm(),
+        "products": Product.objects.all(),
+        'bucket': Bucket.objects.all()
     }
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        bucket_items = Bucket.objects.filter(user=user.id)
+        for item in bucket_items:
+            item.unit_price = item.product.price * item.quantity
+        total_price = sum(item.unit_price for item in bucket_items)
+
+        context['total_price'] = total_price
+
+        return context
 
 
 class ShopDeleteView(PermissionRequiredMixin, DeleteView):
     model = Shop
     pk_url_kwarg = 'id'
+
     def has_permission(self):
         return self.shop.user == self.request.user
 
