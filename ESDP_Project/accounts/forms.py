caca876c02825ea68ced48963c1e026a87537db4
Alexander Shubnikov <sasha.14999933@gmail.com>
2023-11-15 19:29:20 +0600

    #61 delete field

--- ESDP_Project/accounts/forms.py
+++ ESDP_Project/accounts/forms.py
@@ -53,7 +53,6 @@ class RegisterForm(UserCreationForm):
             'email': forms.EmailInput(attrs={'class': 'form-control', 'required': True}),
             'first_name': forms.TextInput(attrs={'class': 'form-control', 'required': True}),
             'last_name': forms.TextInput(attrs={'class': 'form-control', 'required': True}),
-            'phone': forms.NumberInput(attrs={'class': 'form-control'}),
             'password1': forms.PasswordInput(attrs={'class': 'form-control', 'required': True}),
             'password2': forms.PasswordInput(attrs={'class': 'form-control', 'required': True}),
         }
