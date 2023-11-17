07897b2adab65d6e216c866efa405c2e70b686d8
Alexander Shubnikov <sasha.14999933@gmail.com>
2023-11-17 15:57:49 +0600

    I made an endpoint that accepts the user's ID and returns his phone number and fixed the error with the fastapi reload.

--- ESDP_Project/sms-service/main.py
+++ ESDP_Project/sms-service/main.py
@@ -1,4 +1,7 @@
 import fastapi
+from fastapi import Depends, HTTPException
+from sqlalchemy import create_engine, Column, Integer, String, text
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
 from starlette import status
 
 import dto
@@ -6,6 +9,43 @@ from adapters import SMSAdapter
 
 
 app = fastapi.FastAPI()
+
+DATABASE_URL = 'postgresql://postgres:postgres@postgres/shop'
+
+engine = create_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+class Item(Base):
+    __tablename__ = 'accounts_user'
+    id = Column(Integer, primary_key=True, index=True)
+    phone = Column(String)
+
+
+Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get('/items/{item_id}')
+async def read_item(item_id: int, db: Session = Depends(get_db)):
+    query = text('SELECT id, phone FROM accounts_user WHERE id = :item_id')
+    result = db.execute(query, {'item_id': item_id})
+    item = result.fetchone()
+    print(item)
+
+    if item is None:
+        raise HTTPException(status_code=404, detail='Item not found')
+
+    return {'id': item.id, "phone": item.phone}
 
 
 @app.post('/sms/send')
@@ -17,5 +57,5 @@ async def send_sms(body: dto.SendSmsDto):
 
 if __name__ == '__main__':
     import uvicorn
-    
-    uvicorn.run('main:app', host='0.0.0.0', port=1026, reload=True)
+
+    uvicorn.run('main:app',  host='0.0.0.0', port=1026, reload=True)
