22ed59b5d5095d080fe1dad50f34f2edd80d9a35
Valeria Malina <wollt1362@gmail.com>
2023-11-29 16:37:59 +0600

    fix merge

--- ESDP_Project/api/views.py
+++ ESDP_Project/api/views.py
@@ -14,8 +14,11 @@ from accounts.models import Account
 from shop.models import TimeDiscount, Product, Bucket, Order, OrderProducts
 from shop.views import get_client_ip
 from shop.views.bucket import BucketListView
+
+from shop.views.additional_functions import get_discount
 from .serializers import TimeDiscountSerializer, BucketSerializer, ProductSerializer, OrderSerializer, OrderIdSerializer
 import requests
+
 
 class LogoutView(APIView):
 
@@ -114,24 +117,10 @@ class BucketViewSet(viewsets.ModelViewSet):
             created = Bucket.objects.create(ip_address=ip_address, product_id=product_id, shop_id=shop_id,
                                             quantity=quantity)
 
-        self.get_discount(created)
+        get_discount(created)
         serializer = self.get_serializer(created)
 
         return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
-
-    @staticmethod
-    def get_discount(item) -> None:
-        product = item.product
-        quantity = decimal.Decimal(item.quantity)
-        item.unit_price = product.price * quantity
-
-        if discount := product.discounted_price:
-            item.unit_price = discount * quantity
-
-        if TimeDiscount.objects.filter(product=product).exists():
-            time_discount = TimeDiscount.objects.get(product=product)
-            item.unit_price = time_discount.discounted_price * quantity
-        item.save()
 
     @action(detail=True, methods=['DELETE'])
     def remove_from_cart(self, request, *args, **kwargs):
@@ -190,12 +179,15 @@ class OrderViewSet(viewsets.ModelViewSet):
         order.save()
 
         if account := data.get('account'):
-            order.payer = Account.objects.get(id=account)
+            order.account = Account.objects.get(id=account)
             order.save()
             return JsonResponse(data={'order_id': order.id, 'user_id': account}, status=status.HTTP_201_CREATED)
+        else:
+            ip = get_client_ip(request)
+            order.user_ip = ip
+            order.save()
 
         return JsonResponse(data={'order_id': order.id}, status=status.HTTP_201_CREATED)
-
     @staticmethod
     def add_products(products, order):
         for item in products:
@@ -208,7 +200,7 @@ class OrderViewSet(viewsets.ModelViewSet):
             item.delete()
 
 
-class Create_check(APIView):
+class CreateCheck(APIView):
     def get_api_token(self):
         url = settings.URL_REKASSA + 'auth/login'
         api_key = settings.TOKEN_REKASSA
@@ -224,10 +216,9 @@ class Create_check(APIView):
         except requests.RequestException as e:
             raise ValueError(f"Error getting API token: {e}")
 
-
     def register_ticket(self, api_token, order_products):
         url = settings.URL_REKASSA + 'crs/1316/tickets'
-        items= []
+        items = []
         total_bills = 0
         for op in order_products:
             product = op.product
