adf25f3eefd5fb3c04d96c1af6c2f956eb2f5fa7
Gleb <neloxchestno@gmail.com>
2024-01-06 01:14:21 +0600

    A little bit fix bug with login form on main page

--- core/static/js/login.js
+++ core/static/js/login.js
@@ -1,1 +1,10 @@
-console.log('lox')
+
+let errors = $('.alert').length > 0
+
+if (errors){
+   $('.drop').addClass('show marg-form-dropdown')
+}
+
+$('#dropLink').click(function (){
+    $('.drop').removeClass('marg-form-dropdown')
+})
