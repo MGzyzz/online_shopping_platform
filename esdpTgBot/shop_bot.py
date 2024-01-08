cda4015cecc1a77501e25946f3b1da2fe47296ec
Alexander Shubnikov <sasha.14999933@gmail.com>
2024-01-08 13:27:52 +0600

    work with bot

--- esdpTgBot/shop_bot.py
+++ esdpTgBot/shop_bot.py
@@ -40,7 +40,7 @@ class ShopBot:
                         url="https://market.shopuchet.kz/static/telegram.html"), data=self.shop_data['products'])
                 ],
 
-                [types.InlineKeyboardButton(text="Перейти на наш сайт", url=f'https://market.shopuchet.kz/{self.shop_data["id"]}')]
+                [types.InlineKeyboardButton(text="Перейти на наш сайт", url=f'https://market.shopuchet.kz/shop/{self.shop_data["id"]}')]
             ]
             keyboard = types.InlineKeyboardMarkup(inline_keyboard=buttons)
 
