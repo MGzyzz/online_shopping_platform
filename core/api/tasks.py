b824063b94c84fd0346d09f189ce84786af8b685
Gleb <neloxchestno@gmail.com>
2024-01-10 18:29:19 +0600

    Made delete celery metadata from redis

--- core/api/tasks.py
+++ core/api/tasks.py
@@ -1,7 +1,14 @@
+import redis
 from celery import shared_task
 from django.utils import timezone
 
 from shop.models import TimeDiscount
+
+
+def delete_keys_with_prefix(redis_client, prefix):
+    keys_to_delete = redis_client.keys(f'{prefix}*')
+    for key in keys_to_delete:
+        redis_client.delete(key)
 
 
 @shared_task
@@ -14,3 +21,13 @@ def check_expiration():
 
         if now >= end_date:
             discount.delete()
+
+
+@shared_task
+def cleanup_old_task_metadata():
+    redis_client = redis.StrictRedis(host='redis', port=6379, db=0)
+    keys_to_delete = redis_client.keys('celery-task-meta-*')
+
+    for key in keys_to_delete:
+        redis_client.delete(key)
+
