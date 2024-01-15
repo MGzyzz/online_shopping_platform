2616f8f2b263b914cd4bc2ebc0468275b9f05fe9
Gleb <neloxchestno@gmail.com>
2024-01-15 15:43:54 +0600

    Fix sms-service

--- sms-service/main.py
+++ sms-service/main.py
@@ -39,4 +39,4 @@ async def send_sms(id_: int):
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run('main:app', host='0.0.0.0', port=1026, reload=True)
+    uvicorn.run('main:app', host='0.0.0.0', port=1026, reload=False)
